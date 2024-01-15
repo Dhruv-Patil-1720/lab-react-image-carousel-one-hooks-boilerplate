0fd6783cf64d4a26d2bc9db2542ea00f54059e8c
Dhruv Unmesh Patil <universo_esplorar@Dhruvs-MacBook-Air.local>
2024-01-15 15:59:38 +0530

    done

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,12 +2,39 @@ import React, { useState } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
 
-// you can research - how to use material ui
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-// complete the function below:
 function Carousel() {
+    const [currindex, setcurrIndex] = useState(0);
+
+    const handleNext = () => {
+        setcurrIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+
+    const handlePrev = () => {
+        setcurrIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+    };
+
+    return (
+        <>
+            <div className="mainContainer">
+                <div className="arrowIcon"><ArrowBackIosIcon onClick={handlePrev} /></div>
+                <div className="imageBox">
+                    <img src={images[currindex].img} alt="" />
+                </div>
+                <div className="imageContainer">
+                    <div className="titleBox">
+                        <p>{images[currindex].title}</p>
+                    </div>
+                    <div className="subTitleBox">
+                        <p>{images[currindex].subtitle}</p>
+                    </div>
+                </div>
+                <div className="arrowIcon1"><ArrowForwardIosIcon onClick={handleNext} /></div>
+            </div>
+        </>
+    );
 }
 
 export default Carousel;
